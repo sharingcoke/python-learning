9e29675c3b558ced2ce89d22bd7df8d69f183173
sharingcoke <junhuali322@gmail.com>
2023-02-07 17:52:58 +0800

    提交注释

--- first learn/字符串.py
+++ first learn/字符串.py
@@ -1,16 +1,22 @@
-name="""
+name = """
 someone
 somebody
 anyone
 """
-#单，双，三引号都可定义，可以定义多项
+# 单，双，三引号都可定义，可以定义多项
 print(name)
-name='"hello"'
-#字符串中包含引号
+name = '"hello"'
+# 字符串中包含引号
 print(name)
-name="\'world\'"
+name = "\'world\'"
 print(name)
-#字符串内包含引号，可以用转义字符 \
+# 字符串内包含引号，可以用转义字符
 
 
+name_1="something"
+name_2="help me"
+print(name_2+name_1)
 
+a=10
+name="字符串拼接：%d"%(a)
+print(name)
