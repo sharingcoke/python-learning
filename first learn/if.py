c2db9a7dd04d26a8f8421069ccec76ddfef5139a
sharingcoke <junhuali322@gmail.com>
2023-02-09 16:44:09 +0800

    提交注释

--- first learn/if.py
+++ first learn/if.py
@@ -1,7 +1,6 @@
-age = int(input("输入你的年龄:"))
-if age >= 30:
+if int(input("输入你的年龄:")) >= 30:
     print("太老了")
-elif age >= 18:
-    print("成年了")
+elif int(input("输入你的年龄:")) <=0:
+    print("骗我吧")
 else:
     print("你还太年轻了")
